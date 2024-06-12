a06b07659b39fa131b788d2c3e3510ac2c971c25
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-06-12 19:33:59 -0300

    feat: added click listener to button to send message

--- app/src/main/java/com/example/pdmchat/MainActivity.kt
+++ app/src/main/java/com/example/pdmchat/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : AppCompatActivity() {
             setSupportActionBar(this)
         }
 
-
+        amb.sendButton.setOnClickListener {
+            sendMessage()
+        }
     }
 }
